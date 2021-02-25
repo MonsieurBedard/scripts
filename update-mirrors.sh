3bcccbd704607bd8b20d3a7fa209c370a19a2143
Alexandre Bédard <46037007+MonsieurBedard@users.noreply.github.com>
2021-02-25 01:23:12 +0000

    update url in script

--- update-mirrors.sh
+++ update-mirrors.sh
@@ -2,6 +2,6 @@
 
 # Script to update pacman mirrorlist
 
-curl -s "https://www.archlinux.org/mirrorlist/?country=CA&country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+curl -s "https://archlinux.org/mirrorlist/?country=CA&country=US&protocol=http&protocol=https&ip_version=4&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
 cat /etc/pacman.d/mirrorlist
