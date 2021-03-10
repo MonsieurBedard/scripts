53f599e18d8828dd4dd9f5c7aec3a44af277c077
Alexandre Bédard <46037007+MonsieurBedard@users.noreply.github.com>
2021-03-10 14:45:18 -0500

    cleanup

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Simple backup script
-
-TIME=$(date +%Y-%m-%d)
-FILENAME=Backup-$TIME.tar.gz
-SRCDIR=$HOME
-DESDIR=/run/media/alexandre/wd/Backups
-tar -cpzf "$DESDIR"/"$FILENAME" "$SRCDIR"
--- rofi-askpass.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-pass=$(rofi -lines 0 -password -dmenu -input /dev/null -p "Password" -theme-str '#listview {enabled: false;}')
-
-echo "$pass"
